273488ede27a6fa5323df2de2b2661460f68db99
Ezreal <995959149@qq.com>
2020-03-18 23:09:17 +0800

    showhand

---
+++ shell.py
@@ -0,0 +1,33 @@
+import os
+
+# python run.py --model TextCNN --embedding random --word True
+
+# plus = ""
+# plus = " --word True"
+plus = " --word True --embedding random"
+
+
+print("=============== Training Model: CNN ===============\n")
+os.system("python run.py --model TextCNN" + plus)
+#
+# print("=============== Training Model: RNN ===============\n")
+# os.system("python run.py --model TextRNN" + plus)
+#
+# print("=============== Training Model: RNN_ATT ===============\n")
+# os.system("python run.py --model TextRNN_Att" + plus)
+#
+# print("=============== Training Model: RCNN ===============\n")
+# os.system("python run.py --model TextRCNN" + plus)
+
+# print("=============== Training Model: FastText ===============\n")
+# os.system("python run.py --model FastText --embedding random" + plus)
+
+# print("=============== Training Model: DPCNN ===============\n")
+# os.system("python run.py --model DPCNN" + plus)
+#
+# print("=============== Training Model: Transformer ===============\n")
+# os.system("python run.py --model Transformer" + plus)
+
+
+
+
